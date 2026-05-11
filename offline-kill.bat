53f00f3ca037f0390392a313e5f0632584e2df9f
Billy Purdue <billypurdue@users.noreply.github.com>
2026-05-11 08:46:06 -0400

    Update offline-kill.bat

--- offline-kill.bat
+++ offline-kill.bat
@@ -1,40 +1,31 @@
 @echo off
-echo Computrace Kill Script V1.0 - Kippykip, modified to run offline by billypurdue
-echo This is intended to be run from within Sergei Strelec's WinPE, and will fail otherwise!
-echo You can get it here: https://www.majorgeeks.com/files/details/sergei_strelecs_winpe.html
+echo Offline Computrace Kill Script v1.0 by billypurdue
+echo based on
+echo Computrace Kill Script V1.0 - Kippykip
 echo:
 echo This will permanently kill Computrace from this current Windows install using many methods.
 echo There will probably be a lot of errors on missing files / services, 
 echo but that's normal as this script accounts for almost everything Computrace related.
 echo:
-echo Make sure you are running this batch file as administrator!
+echo: This script is intended to be run from sergei strelec's winpe
 echo If you are, then press any key to continue!
 pause > NUL
 echo Disabling Computrace Services...
 
-Reg.exe add "HKLM\SYSTEM\ControlSet001\services\CscService" /v "Start" /t REG_DWORD /d "4" /f
+Reg.exe add "HKLM\_C_comp_system\ControlSet001\services\CscService" /v "Start" /t REG_DWORD /d "4" /f
+Reg.exe add "HKLM\_C_comp_system\ControlSet001\services\Ctes Manager" /v "Start" /t REG_DWORD /d "4" /f
+Reg.exe add "HKLM\_C_comp_system\ControlSet001\services\CtesHostSvc" /v "Start" /t REG_DWORD /d "4" /f
+Reg.exe add "HKLM\_C_comp_system\ControlSet001\services\rpchdp" /v "Start" /t REG_DWORD /d "4" /f
+Reg.exe add "HKLM\_C_comp_system\ControlSet001\services\rpcnet" /v "Start" /t REG_DWORD /d "4" /f
+Reg.exe add "HKLM\_C_comp_system\ControlSet001\services\rpcnetp" /v "Start" /t REG_DWORD /d "4" /f
 
-Reg.exe add "HKLM\SYSTEM\ControlSet001\services\Ctes Manager" /v "Start" /t REG_DWORD /d "4" /f
+Reg.exe add "HKLM\_C_comp_system\ControlSet002\services\CscService" /v "Start" /t REG_DWORD /d "4" /f
+Reg.exe add "HKLM\_C_comp_system\ControlSet002\services\Ctes Manager" /v "Start" /t REG_DWORD /d "4" /f
+Reg.exe add "HKLM\_C_comp_system\ControlSet002\services\CtesHostSvc" /v "Start" /t REG_DWORD /d "4" /f
+Reg.exe add "HKLM\_C_comp_system\ControlSet002\services\rpchdp" /v "Start" /t REG_DWORD /d "4" /f
+Reg.exe add "HKLM\_C_comp_system\ControlSet002\services\rpcnet" /v "Start" /t REG_DWORD /d "4" /f
+Reg.exe add "HKLM\_C_comp_system\ControlSet002\services\rpcnetp" /v "Start" /t REG_DWORD /d "4" /f
 
-Reg.exe add "HKLM\SYSTEM\ControlSet001\services\CtesHostSvc" /v "Start" /t REG_DWORD /d "4" /f
-
-Reg.exe add "HKLM\SYSTEM\ControlSet001\services\rpchdp" /v "Start" /t REG_DWORD /d "4" /f
-
-Reg.exe add "HKLM\SYSTEM\ControlSet001\services\rpcnet" /v "Start" /t REG_DWORD /d "4" /f
-
-Reg.exe add "HKLM\SYSTEM\ControlSet001\services\rpcnetp" /v "Start" /t REG_DWORD /d "4" /f
-
-Reg.exe add "HKLM\SYSTEM\ControlSet002\services\CscService" /v "Start" /t REG_DWORD /d "4" /f
-
-Reg.exe add "HKLM\SYSTEM\ControlSet002\services\Ctes Manager" /v "Start" /t REG_DWORD /d "4" /f
-
-Reg.exe add "HKLM\SYSTEM\ControlSet002\services\CtesHostSvc" /v "Start" /t REG_DWORD /d "4" /f
-
-Reg.exe add "HKLM\SYSTEM\ControlSet002\services\rpchdp" /v "Start" /t REG_DWORD /d "4" /f
-
-Reg.exe add "HKLM\SYSTEM\ControlSet002\services\rpcnet" /v "Start" /t REG_DWORD /d "4" /f
-
-Reg.exe add "HKLM\SYSTEM\ControlSet002\services\rpcnetp" /v "Start" /t REG_DWORD /d "4" /f
 
 echo Deleting Computrace Files...
 DEL /F /Q /A C:\Windows\SysWOW64\cshost.dll
@@ -61,18 +52,21 @@ DEL /F /Q /A C:\Windows\System32\pkgslv.exe
 DEL /F /Q /A C:\Windows\System32\rpcnet.exe
 DEL /F /Q /A C:\Windows\System32\rpcnetp.exe
 DEL /F /Q /A C:\Windows\System32\Upgrd.exe
+
 echo Deleting Computrace Directories...
 RD /S /Q C:\ProgramData\CTES
 RD /S /Q C:\ProgramData\Rpcnet
 echo Disabling Windows WPBT Table...
-reg.exe add "HKLM\SYSTEM\ControlSet001\Control\Session Manager" /v DisableWpbtExecution /t REG_DWORD /d "1" /f
-reg.exe add "HKLM\SYSTEM\ControlSet002\Control\Session Manager" /v DisableWpbtExecution /t REG_DWORD /d "1" /f
+reg.exe add "HKLM\_C_comp_system\ControlSet001\Control\Session Manager" /v DisableWpbtExecution /t REG_DWORD /d "1" /f
+reg.exe add "HKLM\_C_comp_system\ControlSet002\Control\Session Manager" /v DisableWpbtExecution /t REG_DWORD /d "1" /f
+
 echo Blocking Computrace executables via Image File Execution options...
 reg.exe add "HKLM\_C_comp_software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\rpcnetp.exe" /v Debugger /t REG_SZ /d "cmd.exe /c echo Blocked Computrace" /f
 reg.exe add "HKLM\_C_comp_software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\rpcnet.exe" /v Debugger /t REG_SZ /d "cmd.exe /c echo Blocked Computrace" /f
 reg.exe add "HKLM\_C_comp_software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\Upgrd.exe" /v Debugger /t REG_SZ /d "cmd.exe /c echo Blocked Computrace" /f
 reg.exe add "HKLM\_C_comp_software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\instw64.exe" /v Debugger /t REG_SZ /d "cmd.exe /c echo Blocked Computrace" /f
 reg.exe add "HKLM\_C_comp_software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\pkgslv.exe" /v Debugger /t REG_SZ /d "cmd.exe /c echo Blocked Computrace" /f
+
 echo Blocking computrace server communication via hosts file...
 echo 0.0.0.0 websitename.com >> C:\Windows\System32\Drivers\Etc\Hosts
 echo 127.0.0.1 search.namequery.com >> C:\Windows\System32\Drivers\Etc\Hosts
@@ -100,6 +94,7 @@ echo 127.0.0.1 s2.symcb.com >> C:\Windows\System32\Drivers\Etc\Hosts
 echo 127.0.0.1 crl.thawte.com >> C:\Windows\System32\Drivers\Etc\Hosts
 echo 127.0.0.1 cdp.thawte.com >> C:\Windows\System32\Drivers\Etc\Hosts
 echo 127.0.0.1 cacerts.thawte.com >> C:\Windows\System32\Drivers\Etc\Hosts
+
 echo Creating fake directories...
 md C:\Windows\SysWOW64\cshost.dll
 md C:\Windows\SysWOW64\CTLojack.dll
