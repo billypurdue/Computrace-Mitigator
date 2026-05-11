4e4f592dc864e231324520d226ba8748b7c3b137
Billy Purdue <billypurdue@users.noreply.github.com>
2026-05-11 12:30:37 -0400

    offline-kill.bat - now with drive selection

--- offline-kill.bat
+++ offline-kill.bat
@@ -1,124 +1,113 @@
 @echo off
-echo Offline Computrace Kill Script v1.0 by billypurdue
-echo based on
-echo Computrace Kill Script V1.0 - Kippykip
+:: I found this script on github, but it's meant to be run before absolut trips - I modifed it so it can run from Sergei Strelec's WinPE.
+
+setlocal enabledelayedexpansion
+
+echo Offline Computrace Kill Script v1.0 by h0d0r
+echo Based on Kippykip's original script
+
+echo Available volumes/drives:
+
+wmic logicaldisk get deviceid, volumename, size
+
+
+:getdrive
+set /p "DRV=Enter the drive letter of the Windows install (e.g., D): "
+:: Don't need the colons but sometimes I put them by accident
+set "DRV=%DRV::=%"
+
+if not exist "%DRV%:\Windows" (
+    echo [ERROR] %DRV%:\Windows was not found. Please pick a valid Windows install that's not the WinPE boot volume.
+    goto :getdrive
+)
+
 echo:
-echo This will permanently kill Computrace from this current Windows install using many methods.
-echo There will probably be a lot of errors on missing files / services, 
-echo but that's normal as this script accounts for almost everything Computrace related.
-echo:
-echo: This script is intended to be run from sergei strelec's winpe
-echo If you are, then press any key to continue!
+echo Using Target Drive: %DRV%:\
+echo This script is intended to be run from sergei strelec's winpe.
+echo Press any key to start the "Kill" process on %DRV%:\...
 pause > NUL
-echo Disabling Computrace Services...
 
-Reg.exe add "HKLM\_C_comp_system\ControlSet001\services\CscService" /v "Start" /t REG_DWORD /d "4" /f
-Reg.exe add "HKLM\_C_comp_system\ControlSet001\services\Ctes Manager" /v "Start" /t REG_DWORD /d "4" /f
-Reg.exe add "HKLM\_C_comp_system\ControlSet001\services\CtesHostSvc" /v "Start" /t REG_DWORD /d "4" /f
-Reg.exe add "HKLM\_C_comp_system\ControlSet001\services\rpchdp" /v "Start" /t REG_DWORD /d "4" /f
-Reg.exe add "HKLM\_C_comp_system\ControlSet001\services\rpcnet" /v "Start" /t REG_DWORD /d "4" /f
-Reg.exe add "HKLM\_C_comp_system\ControlSet001\services\rpcnetp" /v "Start" /t REG_DWORD /d "4" /f
+echo Disabling Computrace Services by creating and modifying registry keys
+echo Note: Errors during this step indicate either the registry keys already exist (if you've run this before for example) or your Windows volume selection wasn't correct.
+echo .
+Reg.exe add "HKLM\_%DRV%_comp_system\ControlSet001\services\CscService" /v "Start" /t REG_DWORD /d "4" /f
+Reg.exe add "HKLM\_%DRV%_comp_system\ControlSet001\services\Ctes Manager" /v "Start" /t REG_DWORD /d "4" /f
+Reg.exe add "HKLM\_%DRV%_comp_system\ControlSet001\services\CtesHostSvc" /v "Start" /t REG_DWORD /d "4" /f
+Reg.exe add "HKLM\_%DRV%_comp_system\ControlSet001\services\rpchdp" /v "Start" /t REG_DWORD /d "4" /f
+Reg.exe add "HKLM\_%DRV%_comp_system\ControlSet001\services\rpcnet" /v "Start" /t REG_DWORD /d "4" /f
+Reg.exe add "HKLM\_%DRV%_comp_system\ControlSet001\services\rpcnetp" /v "Start" /t REG_DWORD /d "4" /f
 
-Reg.exe add "HKLM\_C_comp_system\ControlSet002\services\CscService" /v "Start" /t REG_DWORD /d "4" /f
-Reg.exe add "HKLM\_C_comp_system\ControlSet002\services\Ctes Manager" /v "Start" /t REG_DWORD /d "4" /f
-Reg.exe add "HKLM\_C_comp_system\ControlSet002\services\CtesHostSvc" /v "Start" /t REG_DWORD /d "4" /f
-Reg.exe add "HKLM\_C_comp_system\ControlSet002\services\rpchdp" /v "Start" /t REG_DWORD /d "4" /f
-Reg.exe add "HKLM\_C_comp_system\ControlSet002\services\rpcnet" /v "Start" /t REG_DWORD /d "4" /f
-Reg.exe add "HKLM\_C_comp_system\ControlSet002\services\rpcnetp" /v "Start" /t REG_DWORD /d "4" /f
-
+Reg.exe add "HKLM\_%DRV%_comp_system\ControlSet002\services\CscService" /v "Start" /t REG_DWORD /d "4" /f
+Reg.exe add "HKLM\_%DRV%_comp_system\ControlSet002\services\Ctes Manager" /v "Start" /t REG_DWORD /d "4" /f
+Reg.exe add "HKLM\_%DRV%_comp_system\ControlSet002\services\CtesHostSvc" /v "Start" /t REG_DWORD /d "4" /f
+Reg.exe add "HKLM\_%DRV%_comp_system\ControlSet002\services\rpchdp" /v "Start" /t REG_DWORD /d "4" /f
+Reg.exe add "HKLM\_%DRV%_comp_system\ControlSet002\services\rpcnet" /v "Start" /t REG_DWORD /d "4" /f
+Reg.exe add "HKLM\_%DRV%_comp_system\ControlSet002\services\rpcnetp" /v "Start" /t REG_DWORD /d "4" /f
 
 echo Deleting Computrace Files...
-DEL /F /Q /A C:\Windows\SysWOW64\cshost.dll
-DEL /F /Q /A C:\Windows\SysWOW64\CTLojack.dll
-DEL /F /Q /A C:\Windows\SysWOW64\DIAGDLL64.DLL
-DEL /F /Q /A C:\Windows\SysWOW64\identprv.dll
-DEL /F /Q /A C:\Windows\SysWOW64\pkgmgr.dll
-DEL /F /Q /A C:\Windows\SysWOW64\pcnet.dll
-DEL /F /Q /A C:\Windows\SysWOW64\wceprv.dll
-DEL /F /Q /A C:\Windows\SysWOW64\instw64.exe
-DEL /F /Q /A C:\Windows\SysWOW64\pkgslv.exe
-DEL /F /Q /A C:\Windows\SysWOW64\rpcnet.exe
-DEL /F /Q /A C:\Windows\SysWOW64\rpcnetp.exe
-DEL /F /Q /A C:\Windows\SysWOW64\Upgrd.exe
-DEL /F /Q /A C:\Windows\System32\cshost.dll
-DEL /F /Q /A C:\Windows\System32\CTLojack.dll
-DEL /F /Q /A C:\Windows\System32\DIAGDLL64.DLL
-DEL /F /Q /A C:\Windows\System32\identprv.dll
-DEL /F /Q /A C:\Windows\System32\pkgmgr.dll
-DEL /F /Q /A C:\Windows\System32\pcnet.dll
-DEL /F /Q /A C:\Windows\System32\wceprv.dll
-DEL /F /Q /A C:\Windows\System32\instw64.exe
-DEL /F /Q /A C:\Windows\System32\pkgslv.exe
-DEL /F /Q /A C:\Windows\System32\rpcnet.exe
-DEL /F /Q /A C:\Windows\System32\rpcnetp.exe
-DEL /F /Q /A C:\Windows\System32\Upgrd.exe
+echo Note: Errors here either mean you've already deleted the files (or they didn't exist) or your Windows volume selection wasn't correct.
+echo .
+for %%F in (cshost.dll CTLojack.dll DIAGDLL64.DLL identprv.dll pkgmgr.dll pcnet.dll wceprv.dll instw64.exe pkgslv.exe rpcnet.exe rpcnetp.exe Upgrd.exe) do (
+    DEL /F /Q /A %DRV%:\Windows\SysWOW64\%%F
+    DEL /F /Q /A %DRV%:\Windows\System32\%%F
+)
 
 echo Deleting Computrace Directories...
-RD /S /Q C:\ProgramData\CTES
-RD /S /Q C:\ProgramData\Rpcnet
-echo Disabling Windows WPBT Table...
-reg.exe add "HKLM\_C_comp_system\ControlSet001\Control\Session Manager" /v DisableWpbtExecution /t REG_DWORD /d "1" /f
-reg.exe add "HKLM\_C_comp_system\ControlSet002\Control\Session Manager" /v DisableWpbtExecution /t REG_DWORD /d "1" /f
+RD /S /Q %DRV%:\ProgramData\CTES
+RD /S /Q %DRV%:\ProgramData\Rpcnet
 
-echo Blocking Computrace executables via Image File Execution options...
-reg.exe add "HKLM\_C_comp_software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\rpcnetp.exe" /v Debugger /t REG_SZ /d "cmd.exe /c echo Blocked Computrace" /f
-reg.exe add "HKLM\_C_comp_software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\rpcnet.exe" /v Debugger /t REG_SZ /d "cmd.exe /c echo Blocked Computrace" /f
-reg.exe add "HKLM\_C_comp_software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\Upgrd.exe" /v Debugger /t REG_SZ /d "cmd.exe /c echo Blocked Computrace" /f
-reg.exe add "HKLM\_C_comp_software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\instw64.exe" /v Debugger /t REG_SZ /d "cmd.exe /c echo Blocked Computrace" /f
-reg.exe add "HKLM\_C_comp_software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\pkgslv.exe" /v Debugger /t REG_SZ /d "cmd.exe /c echo Blocked Computrace" /f
+echo Disabling Windows WPBT Table...
+echo Note: Errors during this step indicate either the registry keys already exist (if you've run this before for example) or your Windows volume selection wasn't correct.
+echo .
+reg.exe add "HKLM\_%DRV%_comp_system\ControlSet001\Control\Session Manager" /v DisableWpbtExecution /t REG_DWORD /d "1" /f
+reg.exe add "HKLM\_%DRV%_comp_system\ControlSet002\Control\Session Manager" /v DisableWpbtExecution /t REG_DWORD /d "1" /f
+
+echo Blocking via Image File Execution options...
+echo Note: Errors during this step indicate either the registry keys already exist (if you've run this before for example) or your Windows volume selection wasn't correct.
+echo .
+for %%E in (rpcnetp.exe rpcnet.exe Upgrd.exe instw64.exe pkgslv.exe) do (
+    reg.exe add "HKLM\_%DRV%_comp_software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\%%E" /v Debugger /t REG_SZ /d "cmd.exe /c echo Blocked Computrace" /f
+)
 
 echo Blocking computrace server communication via hosts file...
-echo 0.0.0.0 websitename.com >> C:\Windows\System32\Drivers\Etc\Hosts
-echo 127.0.0.1 search.namequery.com >> C:\Windows\System32\Drivers\Etc\Hosts
-echo 127.0.0.1 search2.namequery.com >> C:\Windows\System32\Drivers\Etc\Hosts
-echo 127.0.0.1 search64.namequery.com >> C:\Windows\System32\Drivers\Etc\Hosts
-echo 127.0.0.1 eol.absolute.com >> C:\Windows\System32\Drivers\Etc\Hosts
-echo 127.0.0.1 si.namequery.com >> C:\Windows\System32\Drivers\Etc\Hosts
-echo 127.0.0.1 d.namequery.com >> C:\Windows\System32\Drivers\Etc\Hosts
-echo 127.0.0.1 a.fc.namequery.com >> C:\Windows\System32\Drivers\Etc\Hosts
-echo 127.0.0.1 fo.fc.namequery.com >> C:\Windows\System32\Drivers\Etc\Hosts
-echo 127.0.0.1 resources.namequery.com >> C:\Windows\System32\Drivers\Etc\Hosts
-echo 127.0.0.1 cdta.namequery.com >> C:\Windows\System32\Drivers\Etc\Hosts
-echo 127.0.0.1 eum.absolute.com >> C:\Windows\System32\Drivers\Etc\Hosts
-echo 127.0.0.1 api.absolute.com >> C:\Windows\System32\Drivers\Etc\Hosts
-echo 127.0.0.1 ps.namequery.com >> C:\Windows\System32\Drivers\Etc\Hosts
-echo 127.0.0.1 amp.namequery.com >> C:\Windows\System32\Drivers\Etc\Hosts
-echo 127.0.0.1 ps.absolute.com >> C:\Windows\System32\Drivers\Etc\Hosts
-echo 127.0.0.1 ctm.server.absolute.com >> C:\Windows\System32\Drivers\Etc\Hosts
-echo 127.0.0.1 gcm-http.googleapis.com >> C:\Windows\System32\Drivers\Etc\Hosts
-echo 127.0.0.1 bh.namequery.com >> C:\Windows\System32\Drivers\Etc\Hosts
-echo 127.0.0.1 sv.symcb.com >> C:\Windows\System32\Drivers\Etc\Hosts
-echo 127.0.0.1 s.symcb.com >> C:\Windows\System32\Drivers\Etc\Hosts
-echo 127.0.0.1 s1.symcb.com >> C:\Windows\System32\Drivers\Etc\Hosts
-echo 127.0.0.1 s2.symcb.com >> C:\Windows\System32\Drivers\Etc\Hosts
-echo 127.0.0.1 crl.thawte.com >> C:\Windows\System32\Drivers\Etc\Hosts
-echo 127.0.0.1 cdp.thawte.com >> C:\Windows\System32\Drivers\Etc\Hosts
-echo 127.0.0.1 cacerts.thawte.com >> C:\Windows\System32\Drivers\Etc\Hosts
+echo Note: Errors here either likely means that your Windows volume selection wasn't correct.
+echo .
+set "HOSTS=%DRV%:\Windows\System32\Drivers\Etc\Hosts"
+echo 0.0.0.0 websitename.com >> "%HOSTS%"
+echo 127.0.0.1 search.namequery.com >> "%HOSTS%"
+echo 127.0.0.1 search2.namequery.com >> "%HOSTS%"
+echo 127.0.0.1 search64.namequery.com >> "%HOSTS%"
+echo 127.0.0.1 eol.absolute.com >> "%HOSTS%"
+echo 127.0.0.1 si.namequery.com >> "%HOSTS%"
+echo 127.0.0.1 d.namequery.com >> "%HOSTS%"
+echo 127.0.0.1 a.fc.namequery.com >> "%HOSTS%"
+echo 127.0.0.1 fo.fc.namequery.com >> "%HOSTS%"
+echo 127.0.0.1 resources.namequery.com >> "%HOSTS%"
+echo 127.0.0.1 cdta.namequery.com >> "%HOSTS%"
+echo 127.0.0.1 eum.absolute.com >> "%HOSTS%"
+echo 127.0.0.1 api.absolute.com >> "%HOSTS%"
+echo 127.0.0.1 ps.namequery.com >> "%HOSTS%"
+echo 127.0.0.1 amp.namequery.com >> "%HOSTS%"
+echo 127.0.0.1 ps.absolute.com >> "%HOSTS%"
+echo 127.0.0.1 ctm.server.absolute.com >> "%HOSTS%"
+echo 127.0.0.1 gcm-http.googleapis.com >> "%HOSTS%"
+echo 127.0.0.1 bh.namequery.com >> "%HOSTS%"
+echo 127.0.0.1 sv.symcb.com >> "%HOSTS%"
+echo 127.0.0.1 s.symcb.com >> "%HOSTS%"
+echo 127.0.0.1 s1.symcb.com >> "%HOSTS%"
+echo 127.0.0.1 s2.symcb.com >> "%HOSTS%"
+echo 127.0.0.1 crl.thawte.com >> "%HOSTS%"
+echo 127.0.0.1 cdp.thawte.com >> "%HOSTS%"
+echo 127.0.0.1 cacerts.thawte.com >> "%HOSTS%"
 
-echo Creating fake directories...
-md C:\Windows\SysWOW64\cshost.dll
-md C:\Windows\SysWOW64\CTLojack.dll
-md C:\Windows\SysWOW64\DIAGDLL64.DLL
-md C:\Windows\SysWOW64\identprv.dll
-md C:\Windows\SysWOW64\pkgmgr.dll
-md C:\Windows\SysWOW64\pcnet.dll
-md C:\Windows\SysWOW64\wceprv.dll
-md C:\Windows\SysWOW64\instw64.exe
-md C:\Windows\SysWOW64\pkgslv.exe
-md C:\Windows\SysWOW64\rpcnet.exe
-md C:\Windows\SysWOW64\rpcnetp.exe
-md C:\Windows\SysWOW64\Upgrd.exe
-md C:\Windows\System32\cshost.dll
-md C:\Windows\System32\CTLojack.dll
-md C:\Windows\System32\DIAGDLL64.DLL
-md C:\Windows\System32\identprv.dll
-md C:\Windows\System32\pkgmgr.dll
-md C:\Windows\System32\pcnet.dll
-md C:\Windows\System32\wceprv.dll
-md C:\Windows\System32\instw64.exe
-md C:\Windows\System32\pkgslv.exe
-md C:\Windows\System32\rpcnet.exe
-md C:\Windows\System32\rpcnetp.exe
-md C:\Windows\System32\Upgrd.exe
-echo All done! Computrace should never show its face again unless you clean install Windows again!
+echo Creating fake directories to block file recreation...
+echo Note: Errors here either mean you've already run this script, or your Windows volume selection wasn't correct.
+echo .
+for %%F in (cshost.dll CTLojack.dll DIAGDLL64.DLL identprv.dll pkgmgr.dll pcnet.dll wceprv.dll instw64.exe pkgslv.exe rpcnet.exe rpcnetp.exe Upgrd.exe) do (
+    md %DRV%:\Windows\SysWOW64\%%F
+    md %DRV%:\Windows\System32\%%F
+)
+
+echo:
+echo All done! Computrace is neutralized on drive %DRV%.
 pause
